ef230e6447973754d92d748fe972d7a68c964b80
XiaoGuoyang <24302010036@m.fudan.edu.cn>
2026-05-14 21:15:45 +0800

    chore: 前端框架配置的修改

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
 
@@ -9,4 +10,10 @@ import './styles/index.scss'
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+
+// 全局注册 Element Plus 图标：路由 meta.icon 与模板里 <component :is="..."/> 可直接按名称使用
+for (const [name, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(name, component)
+}
+
 app.mount('#app')
